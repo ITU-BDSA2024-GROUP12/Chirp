05ebeba67970619913b20beb95b23fdd2d3d8b3c
Rasmus Nielsen <rsni@itu.dk>
2024-09-04 14:51:36 +0200

    Syntax errors fixed

--- Chirp.CLI/SimpleDB/CSVDatabase.cs
+++ Chirp.CLI/SimpleDB/CSVDatabase.cs
@@ -1,8 +1,11 @@
-public class CSVDatabase : IDatabaseRepository
+using SimpleDB;
+
+public class CSVDatabase<T> : IDatabaseRepository<T>
 {
     
-    public IEnumerable<T> Read (int? limit = null){
-        
+    public IEnumerable<T> Read (int? limit = null)
+    {
+        return null;
     }
     
     public void Store(T record){
