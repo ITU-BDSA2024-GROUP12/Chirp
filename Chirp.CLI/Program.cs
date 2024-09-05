d3cdbc7ada04a1742a3ff62ebd469190d5540e3a
Christian Klingenberg Molusson <157725285+ckmolusson@users.noreply.github.com>
2024-09-05 16:03:59 +0200

    Program now uses the read function using Csvhelper

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,7 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
 using DocoptNet;
+using CsvHelper;
+
 try
-{	bool IsWindows = System.OperatingSystem.IsWindows();
+{
+    bool IsWindows = System.OperatingSystem.IsWindows();
     const string Mode = @"Chirp CLI.
 
     Usage:
@@ -10,63 +14,70 @@ try
     ";
     var arguments = new Docopt().Apply(Mode, args, exit: true);
     if (arguments["cheep"].IsTrue)
-    {	string path;
+    {
+        string path;
         var message = arguments["<message>"].ToString();
         var user = Environment.UserName;
         DateTime currentTime = DateTime.UtcNow;
         long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
-        
-        if (IsWindows){
-			path = new(@".\chirp_cli_db.csv");
-		} else {
-			path = new(@"./chirp_cli_db.csv");
-		}
-		
+
+        if (IsWindows)
+        {
+            path = new(@".\chirp_cli_db.csv");
+        }
+        else
+        {
+            path = new(@"./chirp_cli_db.csv");
+        }
+
         using (StreamWriter sw = File.AppendText(path))
         {
             sw.WriteLine($"{user},\"{message}\",{unixTime}");
             sw.Close();
-        }	
-
-    } else if (arguments["read"].IsTrue)
-    {
-        List<string[]> cheeps = new List<string[]>();
-		string filepath;
-        // Open the text file using a stream reader.
-        if (IsWindows){
-			filepath = new(@".\chirp_cli_db.csv");
-		} else {
-			filepath = new(@"./chirp_cli_db.csv");
-		}
-		using StreamReader reader = new(filepath);
-        {
-            string line; 
-            while ((line = reader.ReadLine()) != null)
-            {
-                //Define pattern
-                Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))"); // regex taken from this post on stackoverflow https://stackoverflow.com/questions/3507498/reading-csv-files-using-c-sharp/34265869#34265869
-
-                //Separating columns to array
-                string[] X = CSVParser.Split(line);
-                cheeps.Add(X);
-            }
         }
-        cheeps.RemoveAt(0);
-        foreach (var lines in cheeps)
+
+    }
+    else if (arguments["read"].IsTrue)
+    {
+        string filepath;
+        // Open the text file using a stream reader.
+        if (IsWindows)
         {
-            string message = lines[1];
-            string author = lines[0];
-            DateTime time = UnixTimeStampToDateTime(Double.Parse(lines[2]));
-            Console.WriteLine($"{author} @ {time}: {message}");
+            filepath = new(@".\chirp_cli_db.csv");
+        }
+        else
+        {
+            filepath = new(@"./chirp_cli_db.csv");
+        }
+
+        using (var reader = new StreamReader(filepath))
+        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+        {
+            var records = csv.GetRecords<Cheep>();
+
+            foreach (var lines in records)
+            {
+                DateTime time = UnixTimeStampToDateTime(lines.Timestamp);
+                Console.WriteLine($"{lines.Author} @ {time}: {lines.Message}");
+            }
         }
     }
 }
 catch (Exception e)
-{ } 
+{
+    Console.WriteLine(e.Message);
+} 
 
 DateTime UnixTimeStampToDateTime( double unixTimeStamp ) // Taken from stackoverflow https://stackoverflow.com/questions/249760/how-can-i-convert-a-unix-timestamp-to-datetime-and-vice-versa
 {
     DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
     dateTime = dateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
     return dateTime;
+}
+
+public class Cheep
+{
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public long Timestamp { get; set; }
 }
