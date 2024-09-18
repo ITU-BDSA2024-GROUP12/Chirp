f520d39e74de1f87d1620285728672b16612df70
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-09-18 18:24:29 +0200

    moved filepath

--- src/WebService/Program.cs
+++ src/WebService/Program.cs
@@ -6,21 +6,20 @@ using CsvHelper.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-string path; 
+string path;
+bool IsWindows = System.OperatingSystem.IsWindows();
+if (IsWindows)
+{
+    path = new(@"..\Chirp.CLI\chirp_cli_db.csv");
+}
+else
+{
+    path = new(@"../Chirp.CLI/chirp_cli_db.csv");
+}
 
 app.MapGet("/cheeps", () =>
 {
     //READ
-    bool IsWindows = System.OperatingSystem.IsWindows();
-    if (IsWindows)
-    {
-        path = new(@"..\Chirp.CLI\chirp_cli_db.csv");
-    }
-    else
-    {
-        path = new(@"../Chirp.CLI/chirp_cli_db.csv");
-    }
-    
     var records = new List<Cheep>();
     using (var reader = new StreamReader(path))
     using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
