dc5fe868e71bdab96d44445d9ee204a512e84e74
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-09-18 18:03:36 +0200

    added the webservice ability to read cheeps

---
+++ src/WebService/Program.cs
@@ -0,0 +1,46 @@
+﻿/*using Chirp.CLI;*/
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+string path; 
+
+app.MapGet("/cheeps", () =>
+{
+    //READ
+    bool IsWindows = System.OperatingSystem.IsWindows();
+    if (IsWindows)
+    {
+        path = new(@"..\Chirp.CLI\chirp_cli_db.csv");
+    }
+    else
+    {
+        path = new(@"../Chirp.CLI/chirp_cli_db.csv");
+    }
+    
+    var records = new List<Cheep>();
+    using (var reader = new StreamReader(path))
+    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    {
+        records = csv.GetRecords<Cheep>().ToList();
+
+        return records;
+    }
+});
+
+app.MapPost("/cheep", ( /*Cheep cheep*/) =>
+{
+    
+});
+
+app.Run();
+
+public class Cheep
+{
+    public string Author { get; set; }
+    public string Message { get; set; }
+    public long Timestamp { get; set; }
+}
