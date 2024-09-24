ce0855e0abeba2fb4ace37ebf3f51069b43498a3
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-09-24 21:05:25 +0200

    truely deleting changes ..

--- src/WebService/Program.cs
+++ src/WebService/Program.cs
@@ -8,7 +8,6 @@ var app = builder.Build();
 app.UseDeveloperExceptionPage();
 app.UseStaticFiles();
 app.UseRouting();
-app.MapControllers();
 
 var filePath = Path.Combine(AppContext.BaseDirectory, "staticfiles/chirp_cli_db.csv");
 
