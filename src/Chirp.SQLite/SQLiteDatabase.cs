58acee1e24a13bbb34110d2becdd03013fe9226f
Rasmus Nielsen <rsni@itu.dk>
2024-09-25 15:39:17 +0200

    added RazorPages
    Co-authored-by: Peter <pblo@itu.dk>
    Co-authored-by: Mathias <bamj@itu.dk>
    Co-authored-by: Johannes <johje@itu.dk>

---
+++ src/Chirp.SQLite/SQLiteDatabase.cs
@@ -0,0 +1,41 @@
+﻿namespace Chirp.SQLite;
+
+public class SQLiteDatabase<T> : IDatabaseRepository<T>
+{
+    
+    
+    private SqliteConnnection conn;
+    
+    public SQLiteDatabase ()
+    {
+        //Create connection to the SQLite3 DB file
+        //Using embbeded ressources
+        EmbeddedFileProvider embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+        string sqlDBFilePath = embeddedProvider.GetFileInfo("./data/chirp.db").PhysicalPath;
+        //TODO: If no chirp.db is set via environment variables, then default to ./tmp
+        conn = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+
+        conn.open();
+    }
+
+    public IEnumerable<T> read()
+    {
+        var command = connection.CreateCommand();
+        command.CommandText = sqlQuery;
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            Co
+        }
+    }
+
+    public closeConnection()
+    {
+        conn.close();
+    }
+
+            
+            
+
+}
