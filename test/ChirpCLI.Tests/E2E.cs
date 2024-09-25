d5c74f2811c4770fa0d65feb77d77a81284dc092
Peter Christen Bloch <pblo@itu.dk>
2024-09-25 08:31:02 +0200

    Added end to end tests to verify the program works

---
+++ test/ChirpCLI.Tests/E2E.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+public class ChirpEndToEndTestsUsingProcessStartInfo
+{
+    private readonly string _chirpExecutablePath = "../../../../../src/Chirp.CLI/bin/Debug/net7.0/Chirp.CLI.exe";
+    private readonly string _chirpBaseURL = "http://localhost:5143"; 
+
+    [Fact]
+    public async Task TestCheepCommand()
+    {
+        
+        var cheepMessage = "e2e test successful!";
+        await RunChirpCommand($"cheep \"{cheepMessage}\"");
+
+        // Success is verified by not receiving an error message.
+        
+    }
+
+    [Fact]
+    public async Task TestReadCommand()
+    {
+        
+        var output = await RunChirpCommand("read");
+
+        
+        var expectedCheepMessage = "hello";
+        Assert.Contains(expectedCheepMessage, output);
+    }
+
+    
+    private async Task<string> RunChirpCommand(string arguments)
+    {
+        
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = _chirpExecutablePath, 
+            Arguments = arguments,           
+            RedirectStandardOutput = true,   
+            RedirectStandardError = true,    
+            UseShellExecute = false,         
+            CreateNoWindow = true            
+        };
+
+        
+        using (Process process = new Process { StartInfo = startInfo })
+        {
+            process.Start(); 
+
+            
+            string output = await process.StandardOutput.ReadToEndAsync();
+            string error = await process.StandardError.ReadToEndAsync();
+
+            process.WaitForExit(); 
+
+            if (!string.IsNullOrEmpty(error))
+            {
+                throw new Exception($"Error running Chirp command: {error}");
+            }
+
+            return output;
+        }
+    }
+}
