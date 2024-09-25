706f384e271ba5bb4aa1a8d3714959032319bd69
Johannes <johje@itu.dk>
2024-09-25 15:55:28 +0200

    Changed using statements, moved SQLite3 project import to Chirp.SQLite.csproj, renamed SQLiteDatabase.cs to DBFacade.cs

--- src/Chirp.SQLite/DBFacade.cs
+++ src/Chirp.SQLite/DBFacade.cs
@@ -1,38 +1,42 @@
-﻿namespace Chirp.SQLite;
+﻿using System.Reflection;
+using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.FileProviders;
 
-public class SQLiteDatabase<T> : IDatabaseRepository<T>
+namespace Chirp.SQLite;
+
+public class DBFacade<T> : IDatabaseRepository<T>
 {
     
     
-    private SqliteConnnection conn;
+    private SqliteConnection conn;
     
-    public SQLiteDatabase ()
+    public DBFacade()
     {
         //Create connection to the SQLite3 DB file
         //Using embbeded ressources
-        EmbeddedFileProvider embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
+        var embeddedProvider = new EmbeddedFileProvider(Assembly.GetExecutingAssembly());
         string sqlDBFilePath = embeddedProvider.GetFileInfo("./data/chirp.db").PhysicalPath;
         //TODO: If no chirp.db is set via environment variables, then default to ./tmp
         conn = new SqliteConnection($"Data Source={sqlDBFilePath}"))
 
-        conn.open();
+        conn.Open();
     }
 
     public IEnumerable<T> read()
     {
-        var command = connection.CreateCommand();
+        var command = conn.CreateCommand();
         command.CommandText = sqlQuery;
 
         using var reader = command.ExecuteReader();
         while (reader.Read())
         {
-            Co
+            
         }
     }
 
-    public closeConnection()
+    public void closeConnection()
     {
-        conn.close();
+        conn.Close();
     }
 
             
