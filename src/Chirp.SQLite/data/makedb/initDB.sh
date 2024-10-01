f63754017324eecd7cdf7f144335e28dbcf18d2f
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-10-01 12:53:03 +0200

    fix linux initDB.sh
    Co-authored-by: Mathias <bamj@itu.dk>

--- src/Chirp.SQLite/data/makedb/initDB.sh
+++ src/Chirp.SQLite/data/makedb/initDB.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Get the directory where the script is located
 #THIS LINE IS GENERATED WITH CHATGPT. (SIDENOTE; I DONT REALLY KNOW BASH NOR WHY EXACTLY IT WORKS... I AM SORRY).
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
