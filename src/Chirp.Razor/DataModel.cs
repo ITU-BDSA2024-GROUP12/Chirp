4559f19f2acd1c793e37a9d3433fdfb5affb1777
Mathias Johnbeck <bamj@itu.dk>
2024-10-02 16:33:16 +0200

    Set cheeps as nullable
    set the cheep field in datamodel as nullable as an author can exist without having any cheeps

--- src/Chirp.Razor/DataModel.cs
+++ src/Chirp.Razor/DataModel.cs
@@ -15,7 +15,7 @@ public class Author
 {
     public string Name { get; set; }
     public string Email { get; set; }
-    public ICollection<Cheep> Cheeps { get; set; }
+    public ICollection<Cheep>? Cheeps { get; set; }
 }
 
 public class CheepDbContext : DbContext
