ebf3313010e0a5f8ab2c53f487b7f19a55725564
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-10-04 11:31:13 +0200

    removed SQLite (???)
    i removed the sqlite project, i dont know if this is the correct thing to do.

--- src/Chirp.SQLite/IDatabaseRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chirp.SQLite;
-
-public interface IDatabaseRepository<T>
-{
-    public IEnumerable<T> Read(int? limit = null);
-    public void Store(IEnumerable<T> record);
-    public IEnumerable<T> ReadFromAuthor(string author, int? limit = null);
-}
-
