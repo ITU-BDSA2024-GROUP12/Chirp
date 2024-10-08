6bde3ce3eeb413cf4775766c947723aa38e639bc
Mathias Johnbeck <bamj@itu.dk>
2024-10-08 13:03:02 +0200

    Added new Migration
    The Migration and database has now been properly updated to use an integer/long as the timestamp so the DTO doesn't need to use DateTime

--- src/Chirp.Razor/Migrations/20241008110048_DateTimeTypeChange.Designer.cs
+++ src/Chirp.Razor/Migrations/20241008110048_DateTimeTypeChange.Designer.cs
@@ -2,6 +2,7 @@
 using DataModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Chirp.Razor.Migrations
 {
     [DbContext(typeof(CheepDbContext))]
-    partial class CheepDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241008110048_DateTimeTypeChange")]
+    partial class DateTimeTypeChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
