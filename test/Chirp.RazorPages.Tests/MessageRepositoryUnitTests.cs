69ab261d6c0b36db47b46f5a44af94a9e0d62587
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-10-21 11:00:58 +0200

    added in memory testing

---
+++ test/Chirp.RazorPages.Tests/MessageRepositoryUnitTests.cs
@@ -0,0 +1,33 @@
+﻿using Chirp.Core;
+using Chirp.Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.RazorPages.Tests;
+
+public class MessageRepositoryUnitTests
+{
+    public MessageRepositoryUnitTests()
+    {
+        
+    }
+
+    [Fact]
+    public async void test()
+    {
+        //Taken from slides: https://github.com/itu-bdsa/lecture_notes/blob/main/sessions/session_07/Slides.md
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<CheepDbContext>().UseSqlite(connection);
+
+        using var context = new CheepDbContext(builder.Options);
+        await context.Database.EnsureCreatedAsync(); // Applies the schema to the database
+
+        DbInitializer.SeedDatabase(context); //empty database otherwise? 
+        
+        ICheepRepository repository = new CheepRepository(context);
+        
+        List<CheepDTO> list = await repository.ReadMessagesFromAuthor("Helge", 1);
+        Assert.NotEmpty(list);
+    }
+}
