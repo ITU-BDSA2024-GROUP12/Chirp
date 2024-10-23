56a91fd283e3fd72c130524d328ef7894ef2c169
Johannes <johje@itu.dk>
2024-10-23 10:24:42 +0200

    Added functions to search for authors by email and/or name.

--- src/Chirp.Core/ICheepRepository.cs
+++ src/Chirp.Core/ICheepRepository.cs
@@ -5,6 +5,9 @@ public interface ICheepRepository
     public Task<List<CheepDTO>> ReadMessage(int page);
     public void UpdateMessage();
     public Task<List<CheepDTO>> ReadMessagesFromAuthor(string author, int page);
-    public bool CreateAuthor(string name, string email);
-    public bool CreateCheep(string author, string email, string text, string time);
+    public bool CreateAuthor(AuthorDTO author);
+    public bool CreateCheep(AuthorDTO author, string text, string time);
+    public Task<AuthorDTO> GetAuthorByEmail(string email);
+    public Task<AuthorDTO> GetAuthorByName(string name);
+    public Task<AuthorDTO> GetAuthor(string name, string email);
 }
