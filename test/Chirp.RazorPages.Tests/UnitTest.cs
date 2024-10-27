50a2dcf3fa1d3b92a923178618efa52fc3c9ae62
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-10-27 13:05:30 +0100

    removed unsued imports

--- test/Chirp.RazorPages.Tests/UnitTest.cs
+++ test/Chirp.RazorPages.Tests/UnitTest.cs
@@ -1,15 +1,4 @@
 using System.Net.Http.Headers;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using Chirp.Infrastructure;
-using Xunit;
-using Microsoft.Extensions.DependencyInjection;
-using Chirp.RazorPages;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
-
 
 namespace Chirp.RazorPages.Tests;
 //https://stackoverflow.com/questions/55131379/integration-testing-asp-net-core-with-net-framework-cant-find-deps-json/70490057#70490057
