ae98caa0ad1c0c79691e5fe27487003739aa3fef
Johannes <johje@itu.dk>
2024-11-05 12:23:54 +0100

    Changed authentication options for Identity to support both local account and github

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -3,6 +3,7 @@ using Chirp.Infrastructure;
 using Chirp.Infrastructure.Data;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<CheepDbContext>(options => options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 //EF Identity
 builder.Services.AddDefaultIdentity<ChirpUser>(options => options.SignIn.RequireConfirmedAccount = true)
@@ -18,9 +20,6 @@ builder.Services.AddDefaultIdentity<ChirpUser>(options => options.SignIn.Require
 //Github OAuth authorization option
 builder.Services.AddAuthentication(options =>
     {
-        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = "GitHub";
     })
     .AddCookie()
     .AddGitHub(o =>
@@ -31,13 +30,13 @@ builder.Services.AddAuthentication(options =>
     });
 
 //Taken from https://stackoverflow.com/questions/31886779/asp-net-mvc-6-aspnet-session-errors-unable-to-resolve-service-for-type
-builder.Services.AddDistributedMemoryCache();
+/*builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options =>
 {
     options.IdleTimeout = TimeSpan.FromSeconds(1800);
-    options.Cookie.HttpOnly = true;
+    options.Cookie.HttpOnly = false;
     options.Cookie.IsEssential = true;
-});
+});*/
 
 
 // Add services to the container.
@@ -51,22 +50,22 @@ using (var scope = app.Services.CreateScope())
 {
     // From the scope, get an instance of our database context.
     // Through the `using` keyword, we make sure to dispose it after we are done.
-    using var context = scope.ServiceProvider.GetService<CheepDbContext>();
+    using CheepDbContext context = scope.ServiceProvider.GetService<CheepDbContext>();
     // Execute the migration from code.
-    if (!app.Environment.IsDevelopment())
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
         context.Database.Migrate();
     }
     
     DbInitializer.SeedDatabase(context);
-}
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -75,7 +74,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseSession();
+//app.UseSession();
 
 app.MapRazorPages();
 
