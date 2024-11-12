824a80813265afcfee3c8e01bd57deaf5e7088b2
Mathias Johnbeck <bamj@itu.dk>
2024-11-12 23:02:58 +0100

    Moved fixture into test file

--- test/Chirp.RazorPages.Tests/UITests.cs
+++ test/Chirp.RazorPages.Tests/UITests.cs
@@ -6,14 +6,52 @@ using NUnit.Framework;
 
 namespace PlaywrightTests;
 
+[SetUpFixture]
+public class TestSetup
+{
+    private Process _webServerProcess;
+    public static string BaseUrl = "http://localhost:3000";
+
+    [OneTimeSetUp]
+    public void GlobalSetup()
+    {
+        // Start the development server
+        _webServerProcess = new Process();
+        _webServerProcess.StartInfo.FileName = "cmd.exe";
+        _webServerProcess.StartInfo.Arguments = "dotnet run";
+        _webServerProcess.StartInfo.WorkingDirectory = @"..\..\src\Chirp.Web";
+        _webServerProcess.StartInfo.CreateNoWindow = true;
+        _webServerProcess.StartInfo.UseShellExecute = false;
+        _webServerProcess.Start();
+            
+        // Optional: Add a wait here if the server takes time to spin up
+        Task.Delay(3000).Wait();
+    }
+
+    [OneTimeTearDown]
+    public void GlobalTeardown()
+    {
+        // Stop the server
+        if (!_webServerProcess.HasExited)
+        {
+            _webServerProcess.Kill();
+        }
+    }
+}
+
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
 public class ExampleTest : PageTest
 {
-	[SetUp]
+
+	private IPlaywright _playwright;
+	private IBrowser _browser;
+
+	[OneTimeSetUp]
 	public async Task SetUp()
 	{
-
+		  _playwright = await Playwright.CreateAsync();
+          _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
 	}
 
     [Test]
@@ -24,9 +62,10 @@ public class ExampleTest : PageTest
 		await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Public Timeline" })).ToBeVisibleAsync();
     } 
 
-	[TearDown]
+	[OneTimeTearDown]
 	public async Task TearDown()
 	{
-
+		 await _browser.CloseAsync();
+         _playwright.Dispose();
 	}
 }
--- test/Chirp.RazorPages.Tests/playwrightFixture.cs
+++ test/Chirp.RazorPages.Tests/playwrightFixture.cs
@@ -4,36 +4,3 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace PlaywrightTests;
-
-[SetUpFixture]
-public class TestSetup
-{
-    private Process _webServerProcess;
-    public static string BaseUrl = "http://localhost:3000";
-
-    [OneTimeSetUp]
-    public void GlobalSetup()
-    {
-        // Start the development server
-        _webServerProcess = new Process();
-        _webServerProcess.StartInfo.FileName = "cmd.exe";
-        _webServerProcess.StartInfo.Arguments = "dotnet run";
-        _webServerProcess.StartInfo.WorkingDirectory = @"..\..\src\Chirp.Web";
-        _webServerProcess.StartInfo.CreateNoWindow = true;
-        _webServerProcess.StartInfo.UseShellExecute = false;
-        _webServerProcess.Start();
-            
-        // Optional: Add a wait here if the server takes time to spin up
-        Task.Delay(3000).Wait();
-    }
-
-    [OneTimeTearDown]
-    public void GlobalTeardown()
-    {
-        // Stop the server
-        if (!_webServerProcess.HasExited)
-        {
-            _webServerProcess.Kill();
-        }
-    }
-}
