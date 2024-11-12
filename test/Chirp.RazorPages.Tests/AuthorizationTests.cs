ec48d7736b24077aa9be71cab6893d50d71b8d9e
Peter Christen Bloch <pblo@itu.dk>
2024-11-12 14:57:22 +0100

    Started work on e2e tests
    -mCo-authored-by: Johannes johje@itu.dk

--- test/Chirp.RazorPages.Tests/AuthorizationTests.cs
+++ test/Chirp.RazorPages.Tests/AuthorizationTests.cs
@@ -1,5 +1,28 @@
+using Chirp.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
 namespace Chirp.RazorPages.Tests;
 
 public class AuthorizationTests
 {
+    private SignInManager<ChirpUser> _signInManager;
+
+    public AuthorizationTests(SignInManager<ChirpUser> signInManager) {
+        
+        {
+            _signInManager = signInManager;
+            
+        }
+    }
+
+    /*[Theory]
+    [InlineData("adho@itu.dk", "M32Want_Access")]
+
+    public async void EndtoEndLogin(string email, string password)
+    {
+        var result = await _signInManager.PasswordSignInAsync(email, password, false, false);
+
+        Assert.True(result.Succeeded);
+    }*/
 }
