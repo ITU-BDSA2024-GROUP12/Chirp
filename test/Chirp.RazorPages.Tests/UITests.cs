2275662b37c7efc1076ba979b3159cc2d477f366
Mathias Johnbeck <bamj@itu.dk>
2024-11-18 00:00:16 +0100

    Register Tests
    Need to fin a way to remove user from database afterwards... WIP

--- test/Chirp.RazorPages.Tests/UITests.cs
+++ test/Chirp.RazorPages.Tests/UITests.cs
@@ -59,6 +59,39 @@ public class ExampleTest : PageTest
         await Expect(Page.Locator("div").Filter(new() { HasText = "Public Timeline Showing 32" }).Nth(1)).ToBeVisibleAsync();
         await Expect(Page.GetByText("Showing 32 cheeps!")).ToBeVisibleAsync();
     }
+    
+     [Test] //tests for basic information that should be visible on public timeline. (un-loggedIn)
+        public async Task registerTest()
+        {
+    		await Page.GotoAsync("http://localhost:5002");
+    		await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "register" })).ToBeVisibleAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
+      		await Page.GetByPlaceholder("name@example.com").ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").FillAsync("name@example.com");
+            await Page.GetByPlaceholder("John Doe").ClickAsync();
+            await Page.GetByPlaceholder("John Doe").FillAsync("testUser1");
+            await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+            await Page.GetByLabel("Password", new() { Exact = true }).ClickAsync();
+            await Page.GetByLabel("Password", new() { Exact = true }).FillAsync("Password123!");
+            await Page.GetByLabel("Confirm Password").ClickAsync();
+            await Page.GetByLabel("Confirm Password").FillAsync("Password123!");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
+            await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Register confirmation" })).ToBeVisibleAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "Click here to confirm your" }).ClickAsync();
+            await Expect(Page.GetByText("Thank you for confirming your")).ToBeVisibleAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
+            await Page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").ClickAsync();
+            await Page.GetByPlaceholder("name@example.com").FillAsync("name@example.com");
+            await Page.GetByPlaceholder("password").ClickAsync();
+            await Page.GetByPlaceholder("password").FillAsync("Password123!");
+            await Page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+            await Expect(Page.GetByRole(AriaRole.Link, new() { Name = "logout [testUser1]" })).ToBeVisibleAsync();
+            await Expect(Page.GetByText("What's on your mind testUser1? Share")).ToBeVisibleAsync();
+        }
+    
+    
+    
 
 }
 
