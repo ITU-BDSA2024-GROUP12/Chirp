a5b5d169897e249b198a00e9487be6433e85fb3b
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-11-22 13:14:46 +0100

    rearranged tests

--- test/Chirp.Infrastructure.Tests/CheepRepositoryTests.cs
+++ test/Chirp.Infrastructure.Tests/CheepRepositoryTests.cs
@@ -31,21 +31,124 @@ public class CheepRepositoryTests
     }
     
     //CreateCheep
+    [Fact]
+    public async Task CreateCheepWhenAuthorExists()
+    {
+        // Arrange
+        var dbContext = GetInMemoryDbContext();
+        var repository = new CheepRepository(dbContext);
+        var author = new Author { AuthorId = 1, Name = "John Testman", Email = "john@test.com" };
+        dbContext.Authors.Add(author);
+        await dbContext.SaveChangesAsync();
+
+        var authorDto = new AuthorDTO { Name = "John Testman", Email = "john@test.com" };
+
+        // Act
+        var result = repository.CreateCheep(authorDto, "Hello World!", "2024-11-05 12:00:00");
+
+        // Assert
+        Assert.True(result);
+        var cheepInDb = await dbContext.Cheeps.FirstOrDefaultAsync(c => c.Text == "Hello World!");
+        Assert.NotNull(cheepInDb);
+        Assert.Equal(author.AuthorId, cheepInDb.AuthorId);
+    }
+    
+    [Fact]
+    public async Task CannotCreateCheepOver160()
+    {
+        // Arrange
+        var author = "test";
+        var email = "test@test.com";
+
+        var sb = new StringBuilder();
+        for (var i = 0; i < 161; i++)
+        {
+            sb.Append('.');
+        }
+
+        var text = sb.ToString();
+            
+        CheepDbContext context = new CheepDbContext(_builder.Options);
+        await context.Database.EnsureCreatedAsync();
+        
+        // Applies the schema to the database
+        DbInitializer.SeedDatabase(context);
+        ICheepRepository repository = new CheepRepository(context);
+        
+        AuthorDTO authorDto = new AuthorDTO()
+        {
+            Name = author,
+            Email = email
+        };
+        // Act & Assert
+        Assert.Throws<InvalidDataException>(() => repository.CreateCheep(authorDto, text, DateTime.Now.ToString()));
+    }
+    
+    [Theory]
+    [InlineData("John John","jojo@itu.dk","Hi everyone!, this is a test!","2024-10-22 13:27:18")]
+    [InlineData("Adrian","adho@itu.dk","This is starting to look like something...","2024-10-22 13:34:53")]
+    public async void CanCreateCheep(string author, string email, string text, string time)
+    {
+        // Arrange
+        CheepDbContext context = new CheepDbContext(_builder.Options);
+        await context.Database.EnsureCreatedAsync();
+        
+        // Applies the schema to the database
+        DbInitializer.SeedDatabase(context);
+        ICheepRepository repository = new CheepRepository(context);
+        
+        AuthorDTO authorDto = new AuthorDTO()
+        {
+            Name = author,
+            Email = email
+        };
+        
+        // Act
+        repository.CreateCheep(authorDto, text, time);
+        
+        
+        //Assert
+        List<CheepDTO> result = repository.GetMessagesFromAuthor(author,0).Result;
+        
+        Assert.Equal(result.First().Text,text);
+    }
+    
     
     //CreateAuthor
+    [Fact]
+    public void CreateAuthorExistingAuthor()
+    {
+        // Arrange
+        var dbContext = GetInMemoryDbContext();
+        var repository = new CheepRepository(dbContext);
+        var author = new Author { Name = "Jane Test", Email = "jane@test.com" };
+        dbContext.Authors.Add(author);
+        dbContext.SaveChanges();
+
+        var authorDto = new AuthorDTO { Name = "Jane Test", Email = "jane@test.com" };
+
+        // Act & Assert
+        var exception = Assert.Throws<Exception>(() => repository.CreateAuthor(authorDto));
+        Assert.Equal("Author Jane Test already exists", exception.Message);
+    }
     
-    //DoesAuthorExist
-    
-    //GetAuthorByEmail
-    
-    //GetAuthorByName
-    
-    //GetAuthor
-    
-    //ReadMessage
-    
-    
-    
+    [Fact]
+    public async Task CreateNonExistingAuthor()
+    {
+        // Arrange
+        var dbContext = GetInMemoryDbContext();
+        var repository = new CheepRepository(dbContext);
+        var authorDto = new AuthorDTO { Name = "John Testman", Email = "john@test.com" };
+
+        // Act
+        var result = repository.CreateAuthor(authorDto);
+
+        // Assert
+        Assert.True(result);
+        var authorInDb = await dbContext.Authors.FirstOrDefaultAsync(a => a.Name == "John Testman");
+        Assert.NotNull(authorInDb);
+        Assert.Equal("john@test.com", authorInDb.Email);
+    }
     
     [Theory]
     [InlineData("Adrian", "adho@itu.dk")]
@@ -94,118 +197,11 @@ public class CheepRepositoryTests
 
         Assert.True(result);
     }
-
     
-    [Theory]
-    [InlineData("John John","jojo@itu.dk","Hi everyone!, this is a test!","2024-10-22 13:27:18")]
-    [InlineData("Adrian","adho@itu.dk","This is starting to look like something...","2024-10-22 13:34:53")]
-    public async void CanCreateCheep(string author, string email, string text, string time)
-    {
-        // Arrange
-        CheepDbContext context = new CheepDbContext(_builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        
-        // Applies the schema to the database
-        DbInitializer.SeedDatabase(context);
-        ICheepRepository repository = new CheepRepository(context);
-        
-        AuthorDTO authorDto = new AuthorDTO()
-        {
-            Name = author,
-            Email = email
-        };
-        
-        // Act
-        repository.CreateCheep(authorDto, text, time);
-        
-        
-        //Assert
-        List<CheepDTO> result = repository.GetMessagesFromAuthor(author,0).Result;
-        
-        Assert.Equal(result.First().Text,text);
-    }
     
-    [Theory]
-    [InlineData("Johannes", "johje@itu.dk")]
-    public async void GetAuthor(string author, string email)
-    {
-        // Arrange
-        CheepDbContext context = new CheepDbContext(_builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        
-        // Applies the schema to the database
-        DbInitializer.SeedDatabase(context);
-        ICheepRepository repository = new CheepRepository(context);
-        
-        await Assert.ThrowsAsync<UserNotFoundException>(() => repository.GetAuthor(author,email));
-    }
-
-    [Fact]
-    public async Task CannotCreateCheepOver160()
-    {
-        // Arrange
-        var author = "test";
-        var email = "test@test.com";
-
-        var sb = new StringBuilder();
-        for (var i = 0; i < 161; i++)
-        {
-            sb.Append('.');
-        }
-
-        var text = sb.ToString();
-            
-        CheepDbContext context = new CheepDbContext(_builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        
-        // Applies the schema to the database
-        DbInitializer.SeedDatabase(context);
-        ICheepRepository repository = new CheepRepository(context);
-        
-        AuthorDTO authorDto = new AuthorDTO()
-        {
-            Name = author,
-            Email = email
-        };
-        // Act & Assert
-        Assert.Throws<InvalidDataException>(() => repository.CreateCheep(authorDto, text, DateTime.Now.ToString()));
-    }
+    //DoesAuthorExist
     
-    [Fact]
-    public async Task CreateNonExistingAuthor()
-    {
-        // Arrange
-        var dbContext = GetInMemoryDbContext();
-        var repository = new CheepRepository(dbContext);
-        var authorDto = new AuthorDTO { Name = "John Testman", Email = "john@test.com" };
-
-        // Act
-        var result = repository.CreateAuthor(authorDto);
-
-        // Assert
-        Assert.True(result);
-        var authorInDb = await dbContext.Authors.FirstOrDefaultAsync(a => a.Name == "John Testman");
-        Assert.NotNull(authorInDb);
-        Assert.Equal("john@test.com", authorInDb.Email);
-    }
-
-    [Fact]
-    public void CreateAuthorExistingAuthor()
-    {
-        // Arrange
-        var dbContext = GetInMemoryDbContext();
-        var repository = new CheepRepository(dbContext);
-        var author = new Author { Name = "Jane Test", Email = "jane@test.com" };
-        dbContext.Authors.Add(author);
-        dbContext.SaveChanges();
-
-        var authorDto = new AuthorDTO { Name = "Jane Test", Email = "jane@test.com" };
-
-        // Act & Assert
-        var exception = Assert.Throws<Exception>(() => repository.CreateAuthor(authorDto));
-        Assert.Equal("Author Jane Test already exists", exception.Message);
-    }
-
+    //GetAuthorByEmail
     [Fact]
     public async Task GetAuthorByEmailWhenAuthorExists()
     {
@@ -223,6 +219,7 @@ public class CheepRepositoryTests
         Assert.NotNull(result);
         Assert.Equal("Jane Test", result.Name);
     }
+    
     /*needs bugfix
     [Fact]
     public async Task GetAuthorByEmailNonExistingAuthor()
@@ -234,30 +231,28 @@ public class CheepRepositoryTests
         // Act & Assert
         await Assert.ThrowsAsync<UserNotFoundException>(() => repository.GetAuthorByEmail("nonexistent@test.com"));
     }*/
-    [Fact]
-    public async Task CreateCheepWhenAuthorExists()
+    
+    //GetAuthorByName
+    
+    //GetAuthor
+    [Theory]
+    [InlineData("Johannes", "johje@itu.dk")]
+    public async void GetAuthor(string author, string email)
     {
         // Arrange
-        var dbContext = GetInMemoryDbContext();
-        var repository = new CheepRepository(dbContext);
-        var author = new Author { AuthorId = 1, Name = "John Testman", Email = "john@test.com" };
-        dbContext.Authors.Add(author);
-        await dbContext.SaveChangesAsync();
-
-        var authorDto = new AuthorDTO { Name = "John Testman", Email = "john@test.com" };
-
-        // Act
-        var result = repository.CreateCheep(authorDto, "Hello World!", "2024-11-05 12:00:00");
-
-        // Assert
-        Assert.True(result);
-        var cheepInDb = await dbContext.Cheeps.FirstOrDefaultAsync(c => c.Text == "Hello World!");
-        Assert.NotNull(cheepInDb);
-        Assert.Equal(author.AuthorId, cheepInDb.AuthorId);
+        CheepDbContext context = new CheepDbContext(_builder.Options);
+        await context.Database.EnsureCreatedAsync();
+        
+        // Applies the schema to the database
+        DbInitializer.SeedDatabase(context);
+        ICheepRepository repository = new CheepRepository(context);
+        
+        await Assert.ThrowsAsync<UserNotFoundException>(() => repository.GetAuthor(author,email));
     }
-
+    
+    //GetMessage
     [Fact]
-    public async Task ReadMessageShouldReturnMessagesPaged()
+    public async Task GetMessageShouldReturnMessagesPaged()
     {
         // Arrange
         var dbContext = GetInMemoryDbContext();
@@ -275,7 +270,8 @@ public class CheepRepositoryTests
         Assert.Single(result);
         Assert.Equal("Hello World!", result[0].Text);
     }
-
+    
+    //GetMessagesFromAuthor
     [Fact]
     public async Task ReadMessagesFromAuthorPaged()
     {
