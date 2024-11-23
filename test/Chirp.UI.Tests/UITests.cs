05dd6051a757d5d7aaac99dc53bd4db49e794765
Mathias Johnbeck <bamj@itu.dk>
2024-11-24 00:45:17 +0100

    Login test Finally FINALLY work

--- test/Chirp.UI.Tests/UITests.cs
+++ test/Chirp.UI.Tests/UITests.cs
@@ -38,7 +38,7 @@ public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task C_SimplePublicTimeLineTest()
     {
-        _output.WriteLine("TestC");
+        _output.WriteLine("Test C failed - attempted to view public timeline");
         // Start Playwright and launch the browser
         var playwright = await Playwright.CreateAsync();
         var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
@@ -59,7 +59,7 @@ public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
 	 [Fact]
     public async Task A_RegisterTest() //Test to register a user, said user is used in later tests
     {
-        _output.WriteLine("TestA");
+        _output.WriteLine("Test A failed - attempted to register a user");
         // Start Playwright and launch the browser
         var playwright = await Playwright.CreateAsync();
         var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
@@ -72,13 +72,13 @@ public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
         await page.GotoAsync(_baseUrl);
         Assert.True(await page.GetByRole(AriaRole.Link, new() { Name = "register" }).IsVisibleAsync());
 		await page.GetByRole(AriaRole.Link, new() { Name = "register" }).ClickAsync();
-		await page.GetByPlaceholder("name@example.com").FillAsync("TestUser1@example.com");
-		await page.GetByPlaceholder("John Doe").FillAsync("TestUser1");
+		await page.GetByPlaceholder("name@example.com").FillAsync("TestUser@example.com");
+		await page.GetByPlaceholder("John Doe").FillAsync("TestUser");
 		await page.GetByLabel("Password", new() { Exact = true }).FillAsync("Password123!");
 		await page.GetByLabel("Confirm Password").FillAsync("Password123!");
 		await page.GetByRole(AriaRole.Button, new() { Name = "Register" }).ClickAsync();
-        Assert.True(await page.GetByRole(AriaRole.Link, new() { Name = "logout [TestUser1]" }).IsVisibleAsync());
-        Assert.True(await page.GetByText("What's on your mind TestUser1? Share").IsVisibleAsync(), "once registered the testUser can cheep");
+        Assert.True(await page.GetByRole(AriaRole.Link, new() { Name = "logout [TestUser]" }).IsVisibleAsync());
+        Assert.True(await page.GetByText("What's on your mind TestUser? Share").IsVisibleAsync(), "once registered the testUser can cheep");
         
         await browser.CloseAsync();
     }
@@ -86,7 +86,7 @@ public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task B_LoginAndOutTest()
     {
-        _output.WriteLine("TestB");
+        _output.WriteLine("Test B failed - Attempted to log in and out");
         // Start Playwright and launch the browser
         var playwright = await Playwright.CreateAsync();
         var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
@@ -98,12 +98,12 @@ public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
         // Navigate to the in-memory server's URL
         await page.GotoAsync(_baseUrl);
         await page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
-        await page.GetByPlaceholder("name@example.com").FillAsync("TestUser1@example.com");
+        await page.GetByPlaceholder("name@example.com").FillAsync("TestUser@example.com");
         await page.GetByPlaceholder("password").FillAsync("Password123!");
         await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
-        Assert.True(await page.GetByRole(AriaRole.Link, new() { Name = "logout [TestUser1]" }).IsVisibleAsync());
-        Assert.True(await page.GetByText("What's on your mind TestUser1? Share").IsVisibleAsync());
-        await page.GetByRole(AriaRole.Link, new() { Name = "logout [TestUser1]" }).ClickAsync();
+        Assert.True(await page.GetByRole(AriaRole.Link, new() { Name = "logout [TestUser]" }).IsVisibleAsync());
+        Assert.True(await page.GetByText("What's on your mind TestUser? Share").IsVisibleAsync());
+        await page.GetByRole(AriaRole.Link, new() { Name = "logout [TestUser]" }).ClickAsync();
         await page.GetByRole(AriaRole.Button, new() { Name = "Click here to Logout" }).ClickAsync();
         Assert.True(await page.GetByText("You have successfully logged").IsVisibleAsync());
         await page.GetByRole(AriaRole.Link, new() { Name = "public timeline" }).ClickAsync();
