60eb753343d3cbcef18bea8d3968fa8599dfec10
Mathias Johnbeck <bamj@itu.dk>
2024-11-25 22:23:28 +0100

    Added DTO for Notifications
    Added DTO abstraction for Notifications table

---
+++ src/Chirp.Core/NotificationDTO.cs
@@ -0,0 +1,11 @@
+namespace Chirp.Core;
+
+public class NotificationDTO
+{
+    public int Id { get; set; } // Notification ID
+    public int CheepId { get; set; } // The Cheep causing the notification
+    public required string CheepContent { get; set; } // Optional: Include content of the Cheep
+    public required string AuthorName { get; set; } // Name of the user who created the notification
+    public bool IsRead { get; set; } // Read/unread status
+    public DateTime Timestamp { get; set; } // Time of the notification
+}
