1436ff404694f559e18f27365a90bc7bafc71c87
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-11-26 17:10:59 +0100

    Author test

--- test/Chirp.Core.Tests/UnitTest.cs
+++ test/Chirp.Core.Tests/UnitTest.cs
@@ -1,10 +1,25 @@
 namespace Chirp.Core.Tests;
 
+
 public class UnitTest
 {
+    //Author DTO
     [Fact]
-    public void Test1()
+    public void AuthorDTOtest()
     {
-
+        //Arrange
+        AuthorDTO authorDto = new AuthorDTO()
+        {
+            AuthorId = 1,
+            Email = "test@test.com",
+            Name = "test"
+        };
+        //Act & Asert
+        
+        Assert.True(authorDto.AuthorId == 1);
+        Assert.True(authorDto.Email == "test@test.com");
+        Assert.True(authorDto.Name == "test");
     }
+    
+    //CheepDTO
 }
