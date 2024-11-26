7f18a9d36e4116b11fb03da4b1aa3d8db31b3938
R03SMUS <87883362+R03SMUS@users.noreply.github.com>
2024-11-26 17:31:26 +0100

    Author type test

--- test/Chirp.Core.Tests/UnitTest.cs
+++ test/Chirp.Core.Tests/UnitTest.cs
@@ -16,9 +16,28 @@ public class UnitTest
         };
         //Act & Asert
         
-        Assert.True(authorDto.AuthorId == 1);
-        Assert.True(authorDto.Email == "test@test.com");
-        Assert.True(authorDto.Name == "test");
+        
+        Assert.Equal(1, authorDto.AuthorId);
+        Assert.Equal("test@test.com", authorDto.Email);
+        Assert.Equal("test", authorDto.Name);
+    }
+    
+    [Fact]
+    public void AuthorDTOtypes()
+    {
+        //Arrange
+        AuthorDTO authorDto = new AuthorDTO()
+        {
+            AuthorId = 1,
+            Email = "test@test.com",
+            Name = "test"
+        };
+        //Act & Asert
+        
+        Assert.IsType(typeof(int), authorDto.AuthorId);
+        Assert.IsType(typeof(string), authorDto.Email);
+        Assert.IsType(typeof(string), authorDto.Name);
+        
     }
     
     //CheepDTO
