e0acaa63be758876cc81091887116d8e37abad7e
Mathias Johnbeck <bamj@itu.dk>
2024-11-27 23:32:10 +0100

    Update to CheepDTo

--- src/Chirp.Core/CheepDTO.cs
+++ src/Chirp.Core/CheepDTO.cs
@@ -9,4 +9,6 @@ public class CheepDTO
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         return dateTime.AddSeconds(TimeStamp).ToString("yyyy-MM-dd HH:mm:ss");
     }
+
+    public List<(string Text, bool IsMention)> HighlightedParts { get; set; } // Add this
 }
