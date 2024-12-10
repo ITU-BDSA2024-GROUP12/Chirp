5cad46666d51fae4f90ab976fbb0b4baa624b207
Johannes <johje@itu.dk>
2024-12-10 13:14:01 +0100

    Added playwright tests for follow and unfollow

--- test/Chirp.UI.Tests/UITests.cs
+++ test/Chirp.UI.Tests/UITests.cs
@@ -21,7 +21,7 @@ namespace PlaywrightTests;
 ///</summery>
 [TestCaseOrderer(
     ordererTypeName: "TestOrdererSetup.TestOrderer.AlphabeticalOrderer ",
-    ordererAssemblyName: "Chirp.UI.Tests")]//runs test in alphabetical order
+    ordererAssemblyName: "Chirp.UI.Tests"),]//runs test in alphabetical order
 public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
 {
     private readonly ITestOutputHelper _output;
@@ -142,6 +142,42 @@ public class PlaywrightTests : IClassFixture<CustomWebApplicationFactory>
         Assert.True(await page.GetByText("TestUser This is a message").IsVisibleAsync());
         //check that the user also can cheep from user timeline
         Assert.True(await page.GetByText("What's on your mind TestUser? Share").IsVisibleAsync());
+        
+        await browser.CloseAsync();
+    }
+    
+    [Fact]
+    public async Task Y_FollowAndUnfollowAuthor()
+    {
+        _output.WriteLine("Test Y failed - Attempted to log in and follow an author");
+        // Start Playwright and launch the browser
+        var playwright = await Playwright.CreateAsync();
+        var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+
+        // Create a new browser context and page
+        var context = await browser.NewContextAsync();
+        var page = await context.NewPageAsync();
+
+        // Navigate to the in-memory server's URL
+        await page.GotoAsync(_baseUrl);
+        //login to be able to follow
+        await page.GetByRole(AriaRole.Link, new() { Name = "login" }).ClickAsync();
+        await page.GetByPlaceholder("name@example.com").FillAsync("TestUser@example.com");
+        await page.GetByPlaceholder("password").FillAsync("Password123!");
+        await page.GetByRole(AriaRole.Button, new() { Name = "Log in" }).ClickAsync();
+        //Follow author
+        await page.GotoAsync(_baseUrl); //Public timeline
+        // Verify Jacqualine Gilcoine exists in the public timeline
+        Assert.True(await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine" }).First.IsVisibleAsync());
+        var li = page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine" }).First;
+        await li.GetByRole(AriaRole.Button, new() { Name = "Follow" }).ClickAsync(); //Follow Author 
+        //Check timeline for message from followed author
+        await page.GetByRole(AriaRole.Link, new() { Name = "my timeline" }).ClickAsync();
+        Assert.True(await page.GetByRole(AriaRole.Heading, new() { Name = "TestUser's Timeline" }).IsVisibleAsync());
+        Assert.True(await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine" }).First.IsVisibleAsync());
+        Assert.True(await page.GetByText("Unfollow").First.IsVisibleAsync());
+        await page.GetByText("Unfollow").First.ClickAsync();
+        Assert.False(await page.Locator("li").Filter(new() { HasText = "Jacqualine Gilcoine" }).First.IsVisibleAsync());
         
         await browser.CloseAsync();
     }
